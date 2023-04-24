a7c0a401f95325b88929abff36db89b9b01497c4
bruno31830 <boissiebruno@gmail.com>
2023-04-24 23:35:58 +0200

    Correctif recursivite correctif initialisation maze

--- Devoir2/src/maze.cpp
+++ Devoir2/src/maze.cpp
@@ -5,7 +5,6 @@ using namespace maze;
 using namespace mazeNode;
 using namespace std;
 
-
 Maze::Maze(const char ascii[16][16], startPoint point)
 {
    // créer les noeuds en fonction du caractère
@@ -26,19 +25,40 @@ Maze::Maze(const char ascii[16][16], startPoint point)
    {
       for (int j = 0; j < 16; j++)
       {
-         if (i > 1)
-            this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Up, this->nodes[i + 1][j]);
+         if (i > 0)
+            this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Up, this->nodes[i - 1][j]);
+         else
+            this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Up, NULL);
          if (i < 15)
-            this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Down, this->nodes[i - 1][j]);
-         if (j > 1)
+            this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Down, this->nodes[i + 1][j]);
+         else
+            this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Down, NULL);
+         if (j > 0)
             this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Left, this->nodes[i][j - 1]);
+         else
+            this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Left, NULL);
          if (j < 15)
             this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Right, this->nodes[i][j + 1]);
+         else
+            this->nodes[i][j]->setNeighbor(mazeNode::NodeNeighbor_Right, NULL);
       }
    }
-   // point de départ
-   this->nodes[point.line][point.col]->setVisited();
-   this->starter = this->nodes[point.line][point.col];
+   if (point.line < 0 || point.line > 15 || point.col < 0 || point.col > 15)
+   {
+      cout << "!!! Warning initialisation maze coordonnées starter !!!" << endl;
+      this->starter = NULL;
+   }
+   else if (this->nodes[point.line][point.col]->getType() != NodeType_Free)
+   {
+      cout << "!!! Warning initialisation maze coordonnées starter case occupée!!!" << endl;
+      this->starter = NULL;
+   }
+   else
+   {
+      // point de départ
+      this->nodes[point.line][point.col]->setVisited();
+      this->starter = this->nodes[point.line][point.col];
+   }
 }
 
 Maze::~Maze()
@@ -48,17 +68,26 @@ Maze::~Maze()
 
 bool Maze::checkConsistency()
 {
+   bool cheeseFind = false;
    // tous les noeuds sont instanciés
    for (int i = 0; i < 16; i++)
    {
       for (int j = 0; j < 16; j++)
       {
-         // si le noeud a un type eNodeType   NodeType_Wall ou NodeType_Free ou NodeType_Cheese
+         // Si le noeud a un type eNodeType   NodeType_Wall ou NodeType_Free ou NodeType_Cheese tout va bien par contre le contraire...
          if ((this->nodes[i][j]->getType() != mazeNode::NodeType_Wall) && (this->nodes[i][j]->getType() != mazeNode::NodeType_Free) && (this->nodes[i][j]->getType() != mazeNode::NodeType_Cheese))
             return false;
+         else if (this->nodes[i][j]->getType() == mazeNode::NodeType_Cheese)
+            cheeseFind = true;
       }
    }
+   // presence d'un fromage
+   if (cheeseFind == false)
+      return false;
+
    // point de départ valide
+   if (this->starter == NULL)
+      return false;
 
    return true;
 }
@@ -83,24 +112,34 @@ bool Maze::parse(mazeNode::Node *node)
 {
    // la récursivité peut-être intéressante pour le parcours d'un arbre de noeuds
    // On va examiner tous les voisins du noeud
-
+   bool ret = false;
    for (int i = NodeNeighbor_Up; i <= NodeNeighbor_Right; i++)
    {
       eNodeNeighbor neighbor = static_cast<eNodeNeighbor>(i);
-      cout  << (node->getNeighbor(neighbor))->getString() << endl;
-      /*
-      if (node->getNeighbor(neighbor)->getType() == mazeNode::NodeType_Cheese)
-      {
-         return true;
-      }
-      
-      else if (node->getNeighbor(neighbor)->getType() == mazeNode::NodeType_Free)
-      {
-         if (!node->getNeighbor(neighbor)->isVisited())
-            return parse(node->getNeighbor(neighbor));
-      }
-      */
-   }
 
-   return true;
+      if (node->isNeighborFree(neighbor))
+      {
+         std::cout << "Node " << node->getString() << " has neighbour free dir " << neighbor << std::endl;
+         char ascii[16][16];
+         this->getAsciiFormat(ascii);
+         for (int line = 0; line < 16; line++)
+         {
+            for (int col = 0; col < 16; col++)
+            {
+               std::cout << ascii[line][col] << " ";
+            }
+            std::cout << std::endl;
+         }
+         // s'il est free on le visite ce qui permet de garder une trace de notre passage
+         //  ce qui permet aussi d'eviter un bouclage infini
+         node->getNeighbor(neighbor)->setVisited();
+         // si c'est le fromage c'est terminé
+         if (node->getNeighbor(neighbor)->getType() == mazeNode::NodeType_Cheese)
+            return true;
+         else
+            // sinon on rappel parse recursivement sur le voisin
+            ret |= this->parse(node->getNeighbor(neighbor));
+      }
+   }
+   return ret;
 }
